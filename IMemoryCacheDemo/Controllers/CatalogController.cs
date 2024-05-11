aacf6836f11677816fd6e39c9f63a158a4a87e28
cilerrsahin@gmail.com <burakc34@gmail.com>
2024-05-12 01:55:45 +0300

    Add project files.

---
+++ IMemoryCacheDemo/Controllers/CatalogController.cs
@@ -0,0 +1,37 @@
+﻿using IMemoryCacheDemo.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace IMemoryCacheDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        List<Product> productList;
+
+        const string cacheKey = "productKey";
+        private readonly IMemoryCache _memoryCache;
+
+        public CatalogController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public class Product
+        {
+            public string Name { get; set; }
+
+            public decimal Price { get; set; }
+        }
+
+        [HttpGet(Name = "GetProducts")]
+        [CustomCache("CachedData", 10)]
+        public IActionResult GetProducts()
+        {
+            productList = new List<Product> { new Product { Name = "Bahce", Price = 4000 }, new Product { Name = "Spor", Price = 10000 } };
+
+            return Ok(productList);
+        }
+    }
+}
---
+++ IMemoryCacheDemo/Helpers/CustomCacheAttribute.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace IMemoryCacheDemo.Helpers
+{
+    public class CustomCacheAttribute : ActionFilterAttribute
+    {
+        private readonly IMemoryCache _cache;
+        private readonly string _cacheKey;
+        private readonly TimeSpan _expiration;
+
+        public CustomCacheAttribute(string cacheKey, int expirationInSeconds)
+        {
+            _cache = new MemoryCache(new MemoryCacheOptions());
+            _cacheKey = cacheKey;
+            _expiration = TimeSpan.FromSeconds(expirationInSeconds);
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (_cache.TryGetValue(_cacheKey, out object cachedValue))
+            {
+                context.Result = new ObjectResult(cachedValue);
+                return;
+            }
+
+            base.OnActionExecuting(context);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (!(_cache.TryGetValue(_cacheKey, out _)))
+            {
+                //Bu kisimda cache onem derecesi ve suresi gibi ayarlamalar yapilmaktadir.
+                var cacheExpOptions = new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpiration = DateTime.Now.AddMinutes(30),
+                    Priority = CacheItemPriority.Normal
+                };
+
+                _cache.Set(_cacheKey, (context.Result as ObjectResult).Value, _expiration);
+            }
+
+            base.OnActionExecuted(context);
+        }
+    }
+}
